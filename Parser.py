e58c5b90517a36d5225b04a7832a57e6272c88c4
elena-krismer <elena@clinical-microbiomics.com>
2020-10-25 00:23:25 +0200

    test

--- Parser.py
+++ Parser.py
@@ -3,7 +3,7 @@ import argparse
 def main():
     parser=argparse.ArgumentParser(description='Convert fast')
     parser.add_argument('-in', help='fasta ', dest='input', type=str)
-    parser.add_argument('-out', help=' fastqoutput filename', dest='output')
+    parser.add_argument('-out', help=' fastqoutput filename', dest='output', type=str)
     parser.add_argument('-qual', help='Quality score')
     parser.set_defaults(func=run)
     args=parser.parse_args()
